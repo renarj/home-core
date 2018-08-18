bf9ed2b608a10c220437a3f50c216e318313b6fc
renarj <renze@renarj.nl>
2018-08-18 23:42:26 +0200

    Fixes for newer cassandra driver, and k8 changes

--- edge-svc/src/main/resources/start.sh
+++ edge-svc/src/main/resources/start.sh
@@ -5,7 +5,7 @@ while true; do
     nc -q 1 -w 5 $amq_host 61616 2>/dev/null && break
 done
 
-echo "Waiting for $mqtt_host to be up and running"
+echo "Waiting for $mqtt_host to be up and running on port $mqtt_port"
 while true; do
     nc -q 1 -w 5 $mqtt_host 1883 2>/dev/null && break
 done
